cfcee58480fc5348f67b1a5ce4283242b141ed17
Dezer <my@ipro.dev>
2024-08-26 21:20:56 +0300

    Add terminal keymap

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,13 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+
+-- Terminal
+map({"n", "t"}, "<C-t>", function ()
+  require("nvchad.term").toggle { pos = "float", id = "floatTerm"}
+end, {desc = "Terminal Toggle Floating term"})
+
+-- map({"n", "t"}, "<A-g>", function ()
+--   require("nvchad.term").toggle { pos = "float", id = "lazygit", clear_cmd = false, cmd = "lazygit"}
+-- end, {desc = "Terminal Toggle Floating term"})
